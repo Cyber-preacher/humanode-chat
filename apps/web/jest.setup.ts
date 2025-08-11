40c114abb182ce5bc064bc746a2077976a3854a7
Dato Kavazi <d@humanode.io>
2025-08-11 16:14:01 +0400

    chore(husky): Windows-safe lint-staged; relax ESLint in tests/mocks; simplify jest.setup

--- apps/web/jest.setup.ts
+++ apps/web/jest.setup.ts
@@ -1,14 +1,8 @@
-// Minimal Node polyfills for WHATWG fetch APIs used by Next server code
-import { fetch, Request, Response, Headers, FormData, File } from "undici";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-// Provide globals if not already there
-// (Node 18+ usually has these, but this keeps Jest predictable)
-if (!(globalThis as any).fetch) (globalThis as any).fetch = fetch as any;
-if (!(globalThis as any).Request) (globalThis as any).Request = Request as any;
-if (!(globalThis as any).Response) (globalThis as any).Response = Response as any;
-if (!(globalThis as any).Headers) (globalThis as any).Headers = Headers as any;
-if (!(globalThis as any).FormData) (globalThis as any).FormData = FormData as any;
-if (!(globalThis as any).File) (globalThis as any).File = File as any;
+import { TextEncoder, TextDecoder } from "node:util";
 
-// Give each test a generous default timeout for async chains
-jest.setTimeout(15000);
+(global as any).TextEncoder = TextEncoder as any;
+(global as any).TextDecoder = TextDecoder as any;
+
+export { };
